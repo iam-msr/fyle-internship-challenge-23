34061b6c7564ce29364812c6b7e328f0c4007794
iam-msr <sriram.mudragada4269+github@gmail.com>
2023-11-25 23:26:03 +0530

    added getRepos function in ApiService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { GithubRepository } from '../github.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,12 @@ export class ApiService {
   constructor(
     private httpClient: HttpClient
   ) { }
-
+  private apiUrl = 'https://api.github.com';
   getUser(githubUsername: string) {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
-
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string, page: number=1, perPage: number=10) {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${perPage}`);
+  }
+  
 }
