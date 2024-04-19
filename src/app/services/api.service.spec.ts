f47d95f61f41b2e6bccd6baa0d02868bc5367418
iam-msr <sriram.mudragada4269+github@gmail.com>
2024-04-19 20:07:07 +0530

    minor changes

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,62 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed,inject } from '@angular/core/testing';
 import { ApiService } from './api.service';
+import { HttpClientTestingModule, HttpTestingController,} from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
+
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should retrieve user information', () => {
+    const dummyUsername = 'testuser';
+    const dummyUserData = { login: dummyUsername, name: 'Test User' };
+
+    service.getUser(dummyUsername).subscribe((userData) => {
+      expect(userData).toEqual(dummyUserData);
+    });
+
+    const req = httpMock.expectOne(
+      `https://api.github.com/users/${dummyUsername}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyUserData);
+  });
+
+  it('should retrieve user repositories with pagination', () => {
+    const dummyUsername = 'testuser';
+    const dummyPage = 1;
+    const dummyPerPage = 10;
+    const dummyUserRepos = [{ name: 'repo1' }, { name: 'repo2' }];
+
+    service
+      .getUserRepos(dummyUsername, dummyPage, dummyPerPage)
+      .subscribe((userRepos) => {
+        expect(userRepos).toEqual(dummyUserRepos);
+      });
+
+    const req = httpMock.expectOne(`https://api.github.com/users/${dummyUsername}/repos?page=${dummyPage}&per_page=${dummyPerPage}`);
+
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyUserRepos);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+  
 });
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
-//rXjs
 import { Observable } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
